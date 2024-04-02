579989ac3d95b25d30d12f89980255966e5e1dd6
TopboySaint <topboysaint1@gmail.com>
2024-04-02 21:55:14 +0100

    OK...Still on it

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ const proceed = () => {
   }
 };
 
-// const addMyBudget = () => {
-function addMyBudget() {
+const addMyBudget = () => {
   let budgetProduct = productName.value;
   let budgetQuantity = productQuantity.value;
   let budgetPrice = productPrice.value;
@@ -19,9 +18,7 @@ function addMyBudget() {
     document.getElementById("msgError").style.display = "block";
   } else {
     myArray.push(budgetObject);
-    // console.log(budgetObject);
     localStorage.setItem("BudgetsValue", JSON.stringify(myArray));
-    // console.log(myArray);
 
     // productName.value === "";
     // productQuantity.value === "";
@@ -29,34 +26,8 @@ function addMyBudget() {
   }
 }
 
-// const addBudget = () => {
-//   let proName = productName.value;
-//   let proQuant = productQuant.value;
-//   let proPrice = productPrice.value;
-//   let proObject = { proName, proQuant, proPrice };
-
-//   if (proName !== "" && proQuant !== "" && proPrice !== "") {
-//     productName.value = "";
-//     productQuant.value = "";
-//     productPrice.value = "";
-
-//     budgetArray.push(proObject);
-//     localStorage.setItem("budget", JSON.stringify(budgetArray));
-//     window.location.href = "trackIt.html";
-//   } else {
-//     err.innerHTML = `<p class="alert alert-danger w-100 col-lg col-sm col-md">fill the input</p>`;
-//   }
-// };
-
-//   if (proName !== "" && proQuant !== "" && proPrice !== "") {
-//     productName.value = "";
-//     productQuant.value = "";
-//     productPrice.value = "";
-
-//     budgetArray.push(proObject);
-//     localStorage.setItem("budget", JSON.stringify(budgetArray));
-//     window.location.href = "trackIt.html";
-//   } else {
-//     err.innerHTML = `<p class="alert alert-danger w-100 col-lg col-sm col-md">fill the input</p>`;
-//   }
-// };
+const enterAmount = () => {
+  let budgetCash = document.getElementById('enterBudget').value
+  budgetCash === ""
+  budgetAmount.innerHTML = `$ ${budgetCash}`
+}
